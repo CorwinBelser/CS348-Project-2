2230c80cc3084064ea359039de723e396e1aaee6
Corwin Belser <cmb539@drexel.edu>
2018-05-07 20:33:28 -0400

    Switched dictionary to use a larger list for now

--- Assets/Scripts/Dictionary/WordManager.cs
+++ Assets/Scripts/Dictionary/WordManager.cs
@@ -12,6 +12,7 @@ public class WordManager : MonoBehaviour {
     private const string DICTIONARY_PATH = "/Resources/Dictionary.xml";
 
     public static WordManager Instance { get; private set; }
+    public const bool LOAD_ALL_WORDS = true;
 
     private void Awake()
     {
@@ -42,21 +43,23 @@ public class WordManager : MonoBehaviour {
         string directory = Application.dataPath + "/Resources/Words/";
         foreach (string path in Directory.GetFiles(directory))
         {
-            if (path.Contains("grade_") && !path.Contains(".meta"))
+            if (LOAD_ALL_WORDS && path.Contains("full_list") && !path.Contains(".meta"))
             {
-                Debug.Log("Loading file: " + path);
+                foreach (string word in System.IO.File.ReadAllLines(path))
+                {
+                    wordList.Add(new Word(word));
+                }
+            }
+            else if (!LOAD_ALL_WORDS && path.Contains("grade_") && !path.Contains(".meta"))
+            {
                 //Get the grade level
                 int gradeLevel = 1;
                 int startIndex = path.LastIndexOf('/') + "/grade_".Length;
                 string sub = path.Substring(startIndex, path.Length - ".txt".Length - startIndex);
-                //Debug.Log("sub: _" + sub + "_");
-                //0123456789
-                //grade_1.txt
                 int.TryParse(sub, out gradeLevel);
-                //Debug.Log("Grade Level: " + gradeLevel);
+
                 foreach(string word in System.IO.File.ReadAllLines(path))
                 {
-                    //Debug.Log("Word: _" + word + "_");
                     wordList.Add(new Word(word, gradeLevel));
                 }
             }
